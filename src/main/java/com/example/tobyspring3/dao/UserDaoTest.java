77c453564fba0b21a543c1d8ccdd84ea5b36fdba
kimmingyun <mingyun8646@nate.com>
2023-05-24 15:48:46 +0900

    fix. tobby-spring[UserDao에서 UserDaoTest로]  - Content :   - UserDaoTest

---
+++ src/main/java/com/example/tobyspring3/dao/UserDaoTest.java
@@ -0,0 +1,20 @@
+package com.example.tobyspring3.dao;
+import com.example.tobyspring3.domain.User;
+
+import java.sql.SQLException;
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        User user = new User();
+        user.setId("9");
+        user.setName("kyeongrok");
+        user.setPassword("1234");
+        userDao.add(user);
+
+        User selectedUser = userDao.get("9");
+        System.out.println(selectedUser.getId());
+        System.out.println(selectedUser.getName());
+        System.out.println(selectedUser.getPassword());
+    }
+}
